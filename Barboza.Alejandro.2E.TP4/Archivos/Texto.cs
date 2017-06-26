432523369e34a732f7598ceb08e52d5f1721ed8e
Alejandro Barboza <barbozaalejandro@outlook.com.ar>
2017-06-26 19:29:46 -0300

    TP N°4

---
+++ Barboza.Alejandro.2E.TP4/Archivos/Texto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string filePath;
+        public Texto(string archivo)
+        {
+            this.filePath = archivo;
+        }
+
+        public bool Guardar(string datos)
+        {
+            bool value = false;
+            using(System.IO.StreamWriter file = new System.IO.StreamWriter(this.filePath, true))
+            {
+                file.WriteLine(datos);
+                value = true;
+            }
+            return value;
+        }
+
+        public bool Leer(out List<string> datos)
+        {
+            bool value = false;
+            datos = new List<string>();
+            using(System.IO.StreamReader file = new System.IO.StreamReader(this.filePath))
+            {
+                while (!file.EndOfStream)
+                {
+                    datos.Add(file.ReadLine());
+                }
+                value = true;
+            }
+            return value;
+        }
+    }
+}
